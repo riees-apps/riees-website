2e96551ec91705c58fd29dcc19a1066d960c69e0
lucasquintino <lucasqf_2@hotmail.com>
2019-06-08 09:48:21 -0300

    Corrigido o caminho absoluto para relativo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Routes from './Routes/index.js'
-import IntlProviderConfigured from 'C:/Users/lucas/React/riees/src/i18n/IntlProviderConfigured.js';
+import IntlProviderConfigured from './i18n/IntlProviderConfigured';
 
 function App() {
   return (
